18bc3c8a34cbcde3b0bcdd1ad2a2d867babc1538
niviwert <niviwert@gmail.com>
2026-03-04 20:32:23 +0200

    in __init__.py and postgres_db.py i did the connection and creation of the db in postgres

---
+++ src/postgres/postgres_db.py
@@ -0,0 +1,27 @@
+import datetime
+import enum
+import logging
+
+import sqlalchemy.orm
+from sqlalchemy import create_engine, schema, String, DateTime, Enum, Sequence
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Status(enum.Enum) :
+    CREATED = 'CREATED',
+    DELETED = 'DELETED'
+
+
+class Bamba(Base):
+    __tablename__ = "bamba"
+
+    id: Mapped[int] = mapped_column(Sequence("bamba_seq_id", start=1), primary_key=True)
+    db_name: Mapped[str] = mapped_column(String(50))
+    status: Mapped[Enum] = mapped_column(Enum(Status))
+    username: Mapped[str] = mapped_column(String(50))
+    creation_time: Mapped[datetime.datetime] = mapped_column(DateTime)
+    #important_identifier: Mapped[str] = mapped_column(active_history=True)
