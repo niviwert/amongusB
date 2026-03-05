1d8750363d8144cd71db4c5a0a949b5d8b7b7af1
niviwert <niviwert@gmail.com>
2026-03-05 13:33:54 +0200

    created deployment.py in app package and added two funcs, trying to figure it out wish me luck...

---
+++ src/api/app/deployment.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Response
+from fastapi.responses import JSONResponse, RedirectResponse
+
+app = FastAPI()
+
+@app.post("/items")
+def add_db(db_name: str, username: str) :
+
+
+@app.get("/portal")
+async def get_portal(teleport: bool = False) -> Response:
+    if teleport:
+        return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    return JSONResponse(content={"message": "Here's your interdimensional portal."})
